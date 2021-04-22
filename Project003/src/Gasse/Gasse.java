b74d229a330ae33b2fd87da9f43ffa22a4adc1cc
mina Shahimian <Amin@DESKTOP-QOVBS7.com>
2021-04-22 15:26:23 +0200

    1.while

---
+++ Project003/src/Gasse/Gasse.java
@@ -0,0 +1,24 @@
+package Gasse;
+
+public class Gasse {
+
+	public static void main(String[] args) {
+		
+
+	int i=0;
+	int summ=1;
+	
+	while(i<10) {
+		summ=1+i;
+		i++;
+		System.out.println("summ: "+summ);
+	}
+
+	
+   // for(i=1;i<100;i++) {
+		
+    //	System.out.println("summ:"+i);
+    //}
+	}
+
+}
