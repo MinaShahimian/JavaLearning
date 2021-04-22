6faae5536fcf34fda833699934630a85bf55e6b1
mina Shahimian <Amin@DESKTOP-QOVBS7.com>
2021-04-22 15:24:15 +0200

    1.for (Multiplication)

---
+++ Project003/src/Einmaleins/Einmaleins.java
@@ -0,0 +1,19 @@
+package Einmaleins;
+
+public class Einmaleins {
+
+	public static void main(String[] args) {
+		
+		int a=1,b;
+		
+		for(a=1;a<=10;a++) {
+			for( b=1;b<=10;b++) 
+				System.out.printf("%2dX%2d=%2d  ",a,b,a*b);
+				System.out.println("");
+			
+			
+		}
+
+	}
+
+}
